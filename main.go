366cd9c6c80307afa8788952eb4c01cce41ecaa8
mbarbita <mbarbita@gmail.com>
2019-10-02 13:16:10 +0300

    Work

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mbarbita/go-sep-tel/servermain"
+
+func main() {
+	servermain.Main()
+}
---
+++ servermain/server_test.go
@@ -0,0 +1,11 @@
+package servermain
+
+import "testing"
+
+func ExampleSalutations() {
+    fmt.Println("hello, and")
+    fmt.Println("goodbye")
+    // Output:
+    // hello, and
+    // goodbye
+}
